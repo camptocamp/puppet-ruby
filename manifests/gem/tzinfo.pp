2ee3746689e5348bc26e42ff2ef68d46b2533290
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2012-12-10 08:52:11 +0100

    added a set of virtual Ruby libraries to install as package or gem

---
+++ manifests/gem/tzinfo.pp
@@ -0,0 +1,19 @@
+# == Class: ruby::gem::tzinfo
+#
+# Declare TZinfo (a Ruby library that uses the standard 
+# Time Zone Database) as virtual resource
+#
+# === Example
+#
+#  include ruby::gems
+#  include ruby::gem::tzinfo
+#  Package <| alias == 'tzinfo' |>
+#
+class ruby::gem::tzinfo {
+
+  @package {'tzinfo':
+    provider => 'gem',
+    alias    => 'tzinfo',
+  }
+
+}
