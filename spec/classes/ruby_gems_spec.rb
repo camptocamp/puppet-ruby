6be94dff38c3f3d16f18ba8a190a123d69be4765
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-11-24 12:25:20 +0100

    Fix unit tests

--- spec/classes/ruby_gems_spec.rb
+++ spec/classes/ruby_gems_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'ruby::gems' do
   let(:facts) { {
-    :lsbdistid => 'Debian',
-    :osfamily  => 'Debian',
+    :operatingsystem => 'Debian',
+    :osfamily        => 'Debian',
   } }
   it { should compile.with_all_deps }
 end
